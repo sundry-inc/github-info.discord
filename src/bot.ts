832248868b1581fd1001981f592a4317231fd018
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-09-01 09:04:55 +0300

    Changed the token

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,7 @@ import { Client, ClientOptions } from "discord.js";
 import dotenv from "dotenv";
 
 dotenv.config()
-const TOKEN = process.env.DISCORD_BOT_TOKEN || ''//'MTAxNDA2NzM3MTM1NjQ1OTAzOQ.GmqibZ.JnelKY_DXxZ1ulsQIhwxhVoCpILawHSojrDQUo';
+const TOKEN = process.env.DISCORD_BOT_TOKEN;
 
 console.log("Bot is starting...")
 
