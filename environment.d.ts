b7aeae836eda9c6cfbdf2c4e42df87853b00628d
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-09-01 20:30:16 +0300

    The bot is connected to mongodb, changes in the license, changes in dependencies, added auto-update of the bot when editing the code

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,8 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DISCORD_BOT_TOKEN: string;
+      MONGODB_CLUSTER_USER: string;
+      MONGODB_CLUSTER_PASS: string;
     }
   }
 }
--- src/bot.ts
+++ src/bot.ts
@@ -1,14 +1,26 @@
+import "dotenv/config";
+
 import { Client, ClientOptions } from "discord.js";
-import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-dotenv.config()
 const TOKEN = process.env.DISCORD_BOT_TOKEN;
+const CLUSTER_PASS = process.env.MONGODB_CLUSTER_PASS;
+const CLUSTER_USER = process.env.MONGODB_CLUSTER_USER;
 
-console.log("Bot is starting...")
+console.log("Bot is starting...");
 
 const client = new Client({
-    intents: []
+	intents: []
 });
+
+client.on('ready', async () => {
+	await mongoose.connect(
+		`mongodb+srv://${CLUSTER_USER}:${CLUSTER_PASS}@cluster0.fki2tsu.mongodb.net/?retryWrites=true&w=majority`,
+		{
+			keepAlive: true, 
+		}
+	)
+})
 client.login(TOKEN);
 
 console.log(client);
