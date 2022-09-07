98d473462689ae3fcee5744e00b4a56e2194a43c
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-09-07 09:12:09 +0300

    Added mongodb connection handling (#5) (#6)
    * Added `ready` listerner (#2)
    
    * Added mongodb connection handling

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -8,14 +8,22 @@ const CLUSTER_USER = process.env.MONGODB_CLUSTER_USER;
 
 export default (client: Client): void => {
   client.on('ready', async () => {
-    await mongoose.connect(
-      `mongodb+srv://${CLUSTER_USER}:${CLUSTER_PASS}@cluster0.fki2tsu.mongodb.net/?retryWrites=true&w=majority`,
-      {
-        keepAlive: true, 
-      }
-    );
-    
     if(!client.user || !client.application) console.error('Oops, client or application error...');    
-    console.log('Success!');
+    
+    let mongodb_conect = false;
+    
+    try {
+      await mongoose.connect(
+        `mongodb+srv://${CLUSTER_USER}:${CLUSTER_PASS}@cluster0.fki2tsu.mongodb.net/?retryWrites=true&w=majority`,
+        {
+          keepAlive: true, 
+        }
+      );
+      mongodb_conect = true;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+    
+    if(mongodb_conect) console.log(`Success! ${client.user?.username} is online`);
   })
 }
