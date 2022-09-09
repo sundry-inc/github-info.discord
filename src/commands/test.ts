1b934d689d516dbaa9737467fe30133c2a14777c
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-09-09 18:39:15 +0300

    cleaning

--- src/commands/test.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "discord.js"
-
-export default {
-  callback: (message: Message, ...args: string[]) => {
-    console.log('Args:' + args);
-    message.reply('test passed!')
-  }
-}
