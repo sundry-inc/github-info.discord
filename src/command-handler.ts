1d4c252659365495fb629df4763334e3acfcba0c
joyfulpebble <96781410+joyfulpebble@users.noreply.github.com>
2022-09-09 18:26:05 +0300

    Decomposition

--- src/command-handler.ts
+++ src/command-handler.ts
@@ -1,33 +1,15 @@
-import  fs, { Dirent } from 'fs';
+// import  fs, { Dirent } from 'fs';
 import { Client } from 'discord.js';
 
-import text_commands from './types/listeners';
-
 import listenerTextCommands from './listeners/text-commands';
+import parseDir from './lib/parse-dir'
 
-function getFiles(path: string, suffix: string): string[] {
-  const files: Dirent[] = fs.readdirSync(path, {
-    withFileTypes: true
-  });
-  let commandFiles: string[] = [];
-
-  for (const file of files) {
-    if(file.isDirectory()){
-      commandFiles = [
-        ...commandFiles,
-        ...getFiles(`${path}/${file.name}`, suffix)
-      ]
-    } else if(file.name.endsWith(suffix))
-      commandFiles.push(`${path}/${file.name}`)
-  }
-
-  return commandFiles;
-}
+import text_commands from './types/listeners';
 
 export default (client: Client) => {
   const commands: text_commands = {};
   const suffix = '.ts';
-  const commandFiles = getFiles(__dirname + '/commands', suffix);
+  const commandFiles = parseDir(__dirname + '/commands', suffix);
 
   for (const command of commandFiles) {
     let commandFile = require(command);
